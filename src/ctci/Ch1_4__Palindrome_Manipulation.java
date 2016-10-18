7103bbbf35e885be0d14b4c2cf5bd6525eeb5ca8
laviathor <aatif.de.shah@gmail.com>
2016-10-18 12:59:59 -0700

    Completed 1.4

--- src/ctci/Ch1_4__Palindrome_Manipulation.java
+++ src/ctci/Ch1_4__Palindrome_Manipulation.java
@@ -18,32 +18,21 @@ import java.util.HashMap;
 public class Ch1_4__Palindrome_Manipulation {
     //Remove spaces and do toLowerCase
     //Check if char count is even, if it is return -1. Otherwise, ja. It can be a palindrome.
-
     private static boolean canBeAPalindrome(String s){
         char[] str = s.toLowerCase().toCharArray();
-        HashMap<Character, Integer> map = new HashMap<>();
         int charCount = 0;
         for(char i : str){
             if(i != ' '){
-                if(map.get(i) == null){
-                    map.put(i, 1);
-                    charCount++;
-                }
-                else{
-                    map.put(i, map.get(i) + 1);
-                    charCount++;
-                }
+                charCount++;
             }
         }
-
-        // An even charCount means no way in hell can it be a palindrome
         return charCount%2 == 0;
     }
 
     public static void main(String[] args){
         System.out.println(canBeAPalindrome("Tact Coa")); //true
         System.out.println(canBeAPalindrome("a ba")); //true
-        System.out.println(canBeAPalindrome("bbbab")); //true
+        System.out.println(canBeAPalindrome("aababab")); //true
         System.out.println(canBeAPalindrome("nope wont work")); //false
     }
 

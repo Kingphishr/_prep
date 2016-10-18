fd2f7d52da808c74316668c46683be5a7aeeb6c9
laviathor <aatif.de.shah@gmail.com>
2016-10-18 13:35:47 -0700

    Completed 1.4

--- src/ctci/Ch1_4__Palindrome_Manipulation.java
+++ src/ctci/Ch1_4__Palindrome_Manipulation.java
@@ -13,24 +13,45 @@
 
 package ctci;
 
+import java.util.HashMap;
+
 public class Ch1_4__Palindrome_Manipulation {
     //Remove spaces and do toLowerCase
-    //Check if char count is even, if it is return -1. Otherwise, ja. It can be a palindrome.
+    //Check if char count is even, if it is return -1.
+    //Check if frequency has only 1 odd charcount, otherwise might be something like "cacbc"
+
     private static boolean canBeAPalindrome(String s){
         char[] str = s.toLowerCase().toCharArray();
-        int charCount = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int charCount = 0, oddCount = 0;
 
-        for(char i : str)
-            if(i != ' ')
-                charCount++;
+        for(char i : str){
+            if(i != ' '){
+                if(map.get(i) == null){
+                    map.put(i, 1);
+                    charCount++;
+                }
+                else{
+                    map.put(i, map.get(i) + 1);
+                    charCount++;
+                }
+            }
+        }
 
-        return charCount%2 != 0;
+        for(char i : str){
+            if(i != ' ' && map.get(i)%2 == 1){
+                oddCount++;
+            }
+        }
+
+        return charCount%2 != 0 && oddCount == 1;
     }
 
     public static void main(String[] args){
         System.out.println(canBeAPalindrome("Tact Coa")); //true
         System.out.println(canBeAPalindrome("a ba")); //true
-        System.out.println(canBeAPalindrome("aababab")); //true
+        System.out.println(canBeAPalindrome("bbbab")); //true
+        System.out.println(canBeAPalindrome("ccc ba")); //false
         System.out.println(canBeAPalindrome("nope wont work")); //false
     }
 

98c59eadb069284f7f58fadc01fb5fab8a3f8300
Aatif Shah <aatif.de.shah@gmail.com>
2016-10-17 22:13:43 -0700

    Completed 1.3

--- src/ctci/Ch1_3__URLify.java
+++ src/ctci/Ch1_3__URLify.java
@@ -1,6 +1,8 @@
 package ctci;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 
+import java.util.Arrays;
 
 /*Question 1.3 - URLify
  * 
@@ -14,11 +16,10 @@ package ctci;
 	
 
 public class Ch1_3__URLify {
-	private static StringBuilder builder = new StringBuilder();
-	
 	
 	// this is my implementation. Dunno why I should use true_length
-	public String urlify_1(String raw, int true_length){
+	private static StringBuilder builder = new StringBuilder();
+	public static String urlify_1(String raw, int true_length){
 		raw = raw.trim();
 		String[] just_words = raw.split(" ");
 		
@@ -34,10 +35,31 @@ public class Ch1_3__URLify {
 		return builder.toString();
 	}
 	
-	//implementations taking advantage of true_length
-	public String urlify_2(String raw, int true_length) {
-		return null;
+	//implementations taking advantage of true_length. This uses less space.
+	public static String urlify_2(String raw, int true_length) {
+		char[] str = raw.toCharArray();
+		int newLength = 1;
+		for(int i = true_length-1; i > 0; i--){
+			if(str[i] == ' '){
+				str[str.length - newLength] = '0';
+				str[str.length - newLength - 1] = '2';
+				str[str.length - newLength -2] = '%';
+				newLength += 3;
+			}
+			else{
+				str[str.length - newLength] = str[i];
+				newLength++;
+			}
+		}
+		return new String(str);
 	}
+	
+	
+	public static void main(String[] args){
+		System.out.println(urlify_1("Mr John Smith    ", 13)); //Pass!
+		System.out.println(urlify_2("Mr John Smith    ", 13)); //Pass!
+	}
+
 }
 
 

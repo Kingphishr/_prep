ae0fbbb4cbc73601428244597301c1ea917c1ab8
Aatif Shah <aatif.de.shah@gmail.com>
2017-08-25 20:42:44 -0700

    Finished problem on page 97, chapter 6

---
+++ chapter_6/page_97.java
@@ -0,0 +1,46 @@
+package chapter_6;
+
+/*
+* PROBLEM
+*
+* Write a function that reverses the order of the words in a string.
+*
+* SOLUTION
+*
+* Complexity = O(n)
+* Space      = O(k) where k is length of remove characters
+*
+*
+* */
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class page_97 {
+
+    String reverseOrder(String input){
+        String[] words = input.split(" ");
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = words.length - 1; i > 0; i--)
+            builder.append(words[i] + " ");
+
+        builder.append(words[0]);
+
+        return builder.toString();
+    }
+
+}
+
+class page_97Test {
+    page_97 test = new page_97();
+
+    @Test
+    void testReverseOrder(){
+        String yoda = "Do or do not, there is no try.";
+        String reverseYoda = "try. no is there not, do or Do";
+
+        Assertions.assertEquals(reverseYoda, test.reverseOrder(yoda));
+    }
+}

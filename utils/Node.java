e08698f3150fed1898e65e5e8448d2006f718e70
Aatif Shah <aatif.de.shah@gmail.com>
2017-09-09 15:55:22 -0700

    Finished 2_3__DeleteMiddleNode

---
+++ utils/Node.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * Created by aatifshah on 9/9/17.
+ */
+public class Node<T> {
+    public T data;
+    public Node next;
+
+    public Node(T d){
+        this.data = d;
+    }
+}

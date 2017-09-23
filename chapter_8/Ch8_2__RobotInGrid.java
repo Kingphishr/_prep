a96a6400c859d5ac64f8e051ea7b0ade268e6d0a
Aatif Shah <aatif.de.shah@gmail.com>
2017-09-22 17:47:05 -0700

    Finished 8.3 Magic Index

---
+++ chapter_8/Ch8_2__RobotInGrid.java
@@ -0,0 +1,14 @@
+package chapter_8;
+
+/**
+ * Created by aatifshah on 9/19/17.
+
+ Robot in a Grid: Imagine a robot sitting on the upper left corner of grid with r rows and c columns.
+ The robot can only move in two directions, right and down, but certain cells are "off limits" such that
+ the robot cannot step on them. Design an algorithm to find a path for the robot from the top left to
+ the bottom right.
+
+ */
+public class Ch8_2__RobotInGrid {
+
+}
--- utils/Stack.java
+++ utils/Stack.java
@@ -3,7 +3,6 @@ package utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testng.asserts.Assertion;
 
 /**
  * Created by aatifshah on 9/19/17.
